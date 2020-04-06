0360f901553a0ee4e32221dddd1f3a2b5deca019
AlpacaFur <luke@bigring.org>
2020-04-06 17:16:32 -0400

    Add a bunch of code

--- server.js
+++ server.js
@@ -1,1 +1,104 @@
-let Discord = require("discord.js");
+const { Client, MessageEmbed } = require("discord.js");
+const fs = require("fs")
+const minecraftPing = require("minecraft-server-util")
+
+var avatarURL;
+var config;
+var statusMessage;
+
+var currentServerData = {};
+
+const client = new Client()
+
+if (fs.existsSync("./config.json")) {
+  config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+}
+else {
+  fatalError("No config file found, exiting!")
+}
+if (!config.token) {
+  fatalError("No token provided!")
+}
+if (!config.serverAddress) {
+  fatalError("No server address provided!")
+}
+
+function fatalError(string) {
+  console.log()
+  console.warn("\x1b[31m"+ string +"\x1b[0m")
+  console.log()
+  process.exit()
+}
+
+function saveConfig() {
+  fs.writeFileSync('./config.json', JSON.stringify(config, null, 2), 'utf8')
+}
+
+function dataToEmbed(error, data) {
+  console.log(data.samplePlayers)
+  let embed = new MessageEmbed()
+    .setAuthor(config.displayAddress || config.serverAddress, avatarURL)
+    .setFooter("Last Updated:")
+    .setTimestamp((new Date()).getTime())
+  if (!error) {
+    embed.setDescription(`Online - Running **${data.version}** \n\n` +
+                         `**Players (${data.samplePlayers.length}):** \n ` +
+                         data.samplePlayers.map(obj=>obj.name).join("\n") +
+                         (data.samplePlayers === 0 ? "No one is on :(": ""))
+    embed.setColor("GREEN")
+  }
+  else {
+    embed.setDescription("Server Offline!")
+    embed.setColor("RED")
+  }
+  return embed;
+}
+
+function startPolling() {
+  setInterval(()=>{
+    minecraftPing(config.serverAddress, config.serverPort, (error, data)=>{
+      statusMessage.edit(dataToEmbed(error, data))
+    })
+  }, 2000)
+}
+
+function selectMessageAndStartPolling(messageId, channelId) {
+  client.channels.fetch(channelId)
+    .then((channel)=>{
+      channel.messages.fetch(messageId)
+        .then((message)=>{
+          statusMessage = message;
+          startPolling();
+        })
+    })
+}
+
+
+if (!config.statusMessageIds) {
+  client.on('message', (msg)=>{
+    if (msg.content === "m!placemessage") {
+      let embed = new MessageEmbed();
+        embed.setDescription("Please wait...")
+      msg.channel.send(embed)
+        .then((message)=>{
+          config.statusMessageIds = [message.id, message.channel.id];
+          saveConfig();
+          selectMessageAndStartPolling(config.statusMessageIds[0],config.statusMessageIds[1]);
+        })
+    }
+  })
+}
+
+client.on('ready', ()=>{
+  console.log("Bot online!");
+  avatarURL = client.user.avatarURL()
+  if (config.statusMessageIds) {
+    selectMessageAndStartPolling(config.statusMessageIds[0],config.statusMessageIds[1]);
+  }
+})
+
+client.login(config.token)
+  .catch(err=>{
+    console.error(err);
+    fatalError("\x1b[31m[ERROR]: Login error, terminating process. Check your token\x1b[0m")
+  })
